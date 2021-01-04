fdf12ef72ccbba48c40fd676e5cebc555bb4a5a3
Keith McKenna <keithmckenna@gmail.com>
2021-01-03 21:07:21 -0600

    Update tachyons for better form UI/UX also added breaking space in text

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,15 +5,15 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
       <p className="f3">
-        {
-          "This Magic Brain will detect faces in your pictures. Go ahead, give it a try!"
-        }
+        {"This Magic Brain will detect faces in your pictures."}
+        <br />
+        {"Go ahead, give it a try!"}
       </p>
       <div className="center">
-        <div className="form pa4 br3 shadow-5">
+        <div className="form pa4 br3 shadow-1">
           <input className="f4 pa2 w-70" type="text" onChange={onInputChange} />
           <button
-            className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple"
+            className="w-30 grow f4 link pv2 dib black bg-washed-green"
             onClick={onButtonSubmit}
           >
             Detect
